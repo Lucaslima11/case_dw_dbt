6d6ff546c67096e0b1d719f2c31a88a8b26d4f00
Lucas Lima <lucaslima111@gmail.com>
2025-08-18 23:23:43 -0300

    Ajuste query

--- case_dw_willbank/models/gold/vw_error_pix_transaction.sql
+++ case_dw_willbank/models/gold/vw_error_pix_transaction.sql
@@ -1,22 +1,12 @@
-with core_transaction as (
+with pix_error_transaction as (
     select
         fc.id_transaction,
         fc.cd_seqlan,
         fc.dt_transaction,
         fc.cd_customer_id,
-        fc.dt_transaction,
-        fc.vl_transaction
+        fc.vl_transaction,
+        rp.is_pix_done
     from {{ ref('tb_fact_core_transaction') }} as fc
-    left join {{ ref('tb_dim_customer_info') }} as ci
-        on ci.cd_customer_id = fc.cd_customer_id
-    where ds_transaction_type = 'PIX ENVIADO'
-),
-pix_transaction as (
-    select
-        fc.id_transaction,
-        fc.cd_seqlan,
-        fc.dt_transaction
-    from {{ ref('tb_fact_pix_transaction') }} as fc
     join {{ ref('rel_core_account_pix') }} rp
         on rp.id_transaction = fc.id_transaction
         and rp.cd_seqlan = fc.cd_seqlan
@@ -25,10 +15,5 @@ pix_transaction as (
 )
 
 select
-    pt.id_transaction,
-    ct.cd_customer_id,
-    ct.dt_transaction,
-    ct.vl_transaction
-from pix_transaction as pt
-left join core_transaction as ct
-    on ct.id_transaction = pt.id_transaction
+    *
+from pix_error_transaction as pt
